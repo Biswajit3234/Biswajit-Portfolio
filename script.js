d887b44edd6e2083ed9fcb1f1d54817c1e1b6767
Biswajit3234 <www.biswajitrout3234@gmail.com>
2023-06-30 03:13:10 +0530

    Changes

--- script.js
+++ script.js
@@ -8,23 +8,25 @@ menuIcon.onclick = () => {
 }
 
 /* =============== Scroll sections active link ==================*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-
+ 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
+ 
+        if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[herf*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
+};
     /* =============== sticky navbar ==================*/
     let header = document.querySelector('header');
 
@@ -33,9 +35,23 @@ window.onscroll = () => {
     /* =============== remove toggle icon navbar ==================*/
 
     menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active')
+    navbar.classList.remove('active');
 
-};
+    /* =============== scroll reveal ==================*/
+
+    ScrollReveal({
+        reset: true,
+        distance:'80px',
+        duration:2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
 var typed = new Typed(".multiple-text", {
     strings: ["Frontend Developer", "Java Developer", "Hacker"],
     typeSpeed: 100,
